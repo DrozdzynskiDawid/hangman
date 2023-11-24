fc4cf6fbaf1dba73f49ba8b6331cc5f17af43310
Dawid <dawid.drozdzynski.g4@gmail.com>
2023-11-24 19:41:06 +0100

    added epoll

--- hangman/config/config.h
+++ hangman/config/config.h
@@ -3,3 +3,5 @@
 #define PLAYER_LIFES 5
 #define GAME_TIME_MINUTES 1
 #define FILE_WITH_WORDS "././config/words.txt"
+#define SERVER_ADDRESS "127.0.0.1"
+#define SERVER_PORT "12345"
--- hangman/server.cpp
+++ hangman/server.cpp
@@ -7,12 +7,40 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/epoll.h>
 #include <unordered_set>
 #include <random>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <error.h>
+#include <signal.h>
 
 using namespace std;
 
 unordered_set<Player*> players;
+int serverFd;
+
+void setReuseAddr(int sock){
+    const int one = 1;
+    int res = setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(one));
+    if(res) error(1,errno,"setsockopt failed");
+}
+
+void prepareServerSocket() {
+    addrinfo hints{};
+    hints.ai_protocol = IPPROTO_TCP;
+    addrinfo *resolved;
+    int res = getaddrinfo(SERVER_ADDRESS, SERVER_PORT, &hints, &resolved);
+    if (res) error(1,errno,"Getaddrinfo failed: %s\n", gai_strerror(res));
+    if (!resolved) error(1,0,"Empty result\n");
+    serverFd = socket(resolved->ai_family, resolved->ai_socktype, resolved->ai_protocol);
+    setReuseAddr(serverFd);
+    int resBind = bind(serverFd, resolved->ai_addr, resolved->ai_addrlen);
+    if (resBind) error(1,errno,"Failed to bind:\n");
+    freeaddrinfo(resolved);
+    int resListen = listen(serverFd, 5);
+    if (resListen) error(1,errno,"Failed to listen");
+}
 
 string getRandomWord() {
     default_random_engine gen((std::random_device()()));
@@ -30,7 +58,7 @@ string getRandomWord() {
         }
     }
     if (lineCounter == 1) {
-        printf("Plik ze słowami jest pusty!");
+        error(1, 0, "Plik ze słowami jest pusty!");
     }
     // getting random word
     uniform_int_distribution<uint8_t> dist(1, lineCounter);
@@ -52,21 +80,75 @@ string getRandomWord() {
     return word;
 }
 
-int main(int argc, char* argv[]) {
-    // testowi gracze
-    players.insert(new Player(44));
-    players.insert(new Player(1000));
+void closeServer() {
+    close(serverFd);
     for (Player* p: players) {
-        if (p->getPlayerFd() == 1000) {
-            p->setNickname("abc");
-        }
-    }
-    for (Player* p: players) {
-        cout << p->getLifes() << p->getNickname() <<p->getPlayerFd()<<endl;
+        shutdown(p->getPlayerFd(), O_RDWR);
         delete p;
     }
-    // koniec testu
+    error(0,0,"Closing server!");
+    exit(1);
+}
+
+void handleCtrlC(int signum) {
+    closeServer();
+}
+
+void handleClient(int fd, epoll_event ee) {
+    if (ee.events & EPOLLIN) {
+        cout << "Message from client: " << ee.data.fd;
+        char buf[256];
+        read(fd,&buf,256);
+        printf(" %s\n", buf);
+        write(fd, "Hello", 5);
+        return;
+    }
+}
+
+void serverLoop() {
+    // epoll
+    int epollFd = epoll_create1(0);
+    epoll_event ee;
+    ee.events = EPOLLIN;
+    ee.data.fd = serverFd;
+    epoll_ctl(epollFd, EPOLL_CTL_ADD, serverFd, &ee);
+
+    while (true) {
+        int number = epoll_wait(epollFd, &ee, 1, -1);
+        if (number == -1) {
+            error(0,errno,"epoll_wait failed");
+        }
+        if (ee.events & EPOLLIN && ee.data.fd == serverFd) {
+            sockaddr_in clientAddr{};
+            socklen_t clientAddrSize = sizeof(clientAddr);
+            int clientFd = accept(serverFd, (sockaddr*) &clientAddr, &clientAddrSize);
+            error(0,0,"clientFd: %d", clientFd);
+            players.insert(new Player(clientFd));
+            epoll_event ee1;
+            ee1.events = EPOLLIN;
+            ee1.data.fd = clientFd;
+            epoll_ctl(epollFd, EPOLL_CTL_ADD, clientFd, &ee1);
+        }
+        else {
+            handleClient(ee.data.fd, ee);
+        }
+        for (Player* p: players) {
+            cout << "Player lifes: " << p->getLifes() << "Player nick" << p->getNickname() << endl;
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    // handle ctrl+c
+    signal(SIGINT, handleCtrlC);
+
+    // server socket creating
+    prepareServerSocket();
+    error(0,0,"serverFd: %d", serverFd);
+
+    // test word
     string word = getRandomWord();
-    cout << "Wylosowane słowo: " << word << endl;
-    cout << "Życia graczy: " << PLAYER_LIFES << endl;;
+    cout << "Random word from file:" << word << endl;
+
+    serverLoop();
 }
